3e8a510f3b881299e87dfa6fc3a7b2bcf019deec
Bledioni <bledionmehmeti4@gmail.com>
2025-09-12 15:35:28 +0200

    Improved UI for Apply Job page

--- frontend/src/components/user/AppliedJobs.jsx
+++ frontend/src/components/user/AppliedJobs.jsx
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { api } from "../..";
+import './style/appliedJobs.css';
+import { useNavigate } from "react-router-dom";
+import UserSideBar from './common/UserSideBar';
+import UserNav from "./common/UserNav";
+
 
 export default function AppliedJobs() {
   const userID = localStorage.getItem("user_id");
   const [jobs, setJobs] = useState([]);
   const [jobDetails, setJobDetails] = useState([]);
+  const navigate = useNavigate();
 
   // 1. Fetch applied jobs
   useEffect(() => {
@@ -48,17 +54,48 @@ export default function AppliedJobs() {
     }
   }, [jobs]);
 
-  return (
-    <div>
-      <h2>Applied Jobs</h2>
-      <ul>
-        {jobDetails.map((job) => (
-          <li key={job.id}>
-            <strong>{job.job_title}</strong> - {job.job_role} <br />
-            <small>Tags: {job.tags}</small>
-          </li>
-        ))}
-      </ul>
+return (
+  <>
+  <UserNav/>
+  <div className="applied-jobs-main-container">
+    <UserSideBar/>
+    <div className="applied-jobs-table-wrapper">
+      <table className="applied-jobs-table">
+        <thead>
+          <tr>
+            <th>Job Title</th>
+            <th>City</th>
+            <th>Tags</th>
+            <th>Min Salary</th>
+            <th>Max Salary</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {jobDetails.map((job) => (
+            <tr key={job.id}>
+              <td>{job.job_title}</td>
+              <td>{job.city}</td>
+              <td>{job.tags}</td>
+              <td>{job.min_salary}</td>
+              <td>{job.max_salary}</td>
+              <td>
+                <button
+                  className="applied-jobs-btn"
+                  onClick={() => navigate(`/jobs/${job.id}`)}
+                >
+                  Job Details
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  );
+  </div>
+  </>
+);
+
+
+
 }

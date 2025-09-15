a35cc9237b9df4973da531b00c61f6e780a9f19e
Bledioni <bledionmehmeti4@gmail.com>
2025-09-15 16:56:54 +0200

    Integrated frontend at findjobapi

--- frontend/src/components/home/HomePage.jsx
+++ frontend/src/components/home/HomePage.jsx
@@ -1,5 +1,8 @@
 import react from "react";
 import NavBar from "./NavBar";
+import FindJobApi from "./api/FindJobApi";
+
+
 
 export default function HomePage(){
 
@@ -7,6 +10,7 @@ export default function HomePage(){
 
         <div>
             <NavBar/>
+            <FindJobApi/>
         </div>
 
     )
--- frontend/src/components/home/api/FindJobApi.jsx
+++ frontend/src/components/home/api/FindJobApi.jsx
@@ -1,8 +1,47 @@
-import React from "react";
-import api from  '../../../index.js';
+import React, { useState } from "react";
+import {api} from "../../../index";
 
-export default function FindJobApi(){
+function FindJobApi() {
+  const [jobTitle, setJobTitle] = useState("");
+  const [city, setCity] = useState("");
 
-    api('')
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    api.post("get-guest-job", {
+      job_title: jobTitle,
+      city: city,
+
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    }).then((res) => {
+      console.log(res.data);
+    });
+  }
+
+  return(
+    <div>
+         <form onSubmit={handleSubmit}>
+            <input 
+                        type="text"
+                        placeholder="Enter A City"
+                        value={city}
+                        onChange={(e) => setCity(e.target.value)}
+                    />
+
+                    <input 
+                        type="text"
+                        placeholder="Enter Job Title"
+                        value={jobTitle}
+                        onChange={(e) => setJobTitle(e.target.value)}
+                    />
+
+                    <button type="submit">Submit</button>
+        </form>
+    </div>
+  )
 
 }
+
+export default FindJobApi;

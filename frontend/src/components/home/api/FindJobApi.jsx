4d50dbe1729ba56c9df53d5d2c2caa55a36dc5db
Bledioni <bledionmehmeti4@gmail.com>
2025-09-16 11:08:39 +0200

    Fixed Ui

--- frontend/src/components/home/api/FindJobApi.jsx
+++ frontend/src/components/home/api/FindJobApi.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import {api} from "../../../index";
+import { api } from "../../../index";
 import { useNavigate } from "react-router-dom";
+import "../style/findJobApi.css";
 
 function FindJobApi() {
   const [jobTitle, setJobTitle] = useState("");
@@ -10,41 +11,53 @@ function FindJobApi() {
   function handleSubmit(e) {
     e.preventDefault();
 
-    api.post("get-guest-job", {
-      job_title: jobTitle,
-      city: city,
+    api
+      .post("get-guest-job", {
+        job_title: jobTitle,
+        city: city,
 
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("token")}`,
-      },
-    }).then((res) => {
-      console.log(res.data);
-      navigate('/find-job-section', { state: { jobs: res.data } });
-    });
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        navigate("/find-job-section", { state: { jobs: res.data } });
+      });
   }
 
-  return(
-    <div>
-         <form onSubmit={handleSubmit}>
-            <input 
-                        type="text"
-                        placeholder="Enter A City"
-                        value={city}
-                        onChange={(e) => setCity(e.target.value)}
-                    />
+  return (
+    <div className="find-job-api-main-section">
+      <div className="find-job-api-find-job-main-section">
+        <div className="find-job-first-section">
+          <h4>Find a job that suits your interest & skills.</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Necessitatibus eaque suscipit itaque obcaecati quod animi voluptate
+            id, excepturi repudiandae. Iure.
+          </p>
+          <div className="find-job-first-section-form-container">
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                placeholder={"Enter A City"}
+                value={city}
+                onChange={(e) => setCity(e.target.value)}
+              />
 
-                    <input 
-                        type="text"
-                        placeholder="Enter Job Title"
-                        value={jobTitle}
-                        onChange={(e) => setJobTitle(e.target.value)}
-                    />
-
-                    <button type="submit">Submit</button>
-        </form>
+              <input
+                type="text"
+                placeholder="Enter Job Title"
+                value={jobTitle}
+                onChange={(e) => setJobTitle(e.target.value)}
+              />
+              <button type="submit">Find Job</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-
+  );
 }
 
 export default FindJobApi;
